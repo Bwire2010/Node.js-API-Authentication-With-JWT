c07d523f3e1a327dce3f13ef1f391c1a727b60fd
Patrobas Bwire <patrobasbwire@gmail.com>
2023-04-05 09:50:54 +0300

    Added Routes

--- index.js
+++ index.js
@@ -2,6 +2,11 @@ const express = require('express');
 const app = express();
 
 
+//Import Routes
+const authRoute = require('./routes/auth');
+
+//Route Middlewares
+app.use('/api/user', authRoute)
 
 
 
