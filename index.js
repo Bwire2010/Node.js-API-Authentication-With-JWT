cdd4f9c42d01b5fe9c38763252ab21a4edfa2e7c
Patrobas Bwire <patrobasbwire@gmail.com>
2023-04-05 12:16:07 +0300

    connected to mongoDB

--- index.js
+++ index.js
@@ -1,5 +1,16 @@
+
 const express = require('express');
 const app = express();
+const dotenv = require('dotenv')
+const mongoose = require ('mongoose');
+
+dotenv.config();
+
+
+//connect to DB
+mongoose.connect( process.env.DB_CONNECT, { useNewUrlParser: true, useUnifiedTopology: true })
+  .then(() => console.log('connected to db!'))
+  .catch(error => console.error(error));
 
 
 //Import Routes
